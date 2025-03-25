0ed963034bc0efd36277be73ed5b9cd6569c6eed
Dyego Lima <dyego.lima@unimedcaruaru.com.br>
2025-03-25 15:50:17 -0300

    Nessa aula aprendemos:
    Como recuperar informações da API através da criação de actions.
    O verbo GET visa retornar recursos da API.
    Como enviar parâmetros através da URL de requisição.
    Como filtrar recursos para retornar para o usuário utilizando LINQ.
    Status 404 indica que um recurso não foi encontrado.
    Como tornar nossos retornos mais enxutos através de conceitos de paginação.
    Como aplicar paginação através dos métodos Skip() e Take().

--- FilmesAPI_Alura/Controllers/FilmeController.cs
+++ FilmesAPI_Alura/Controllers/FilmeController.cs
@@ -8,19 +8,35 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AdicionarFilmeList([FromBody] Filme filme)
+    public IActionResult AdicionarFilmeList([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
+        Console.WriteLine(filme.Titulo+" adicionado a lista.");
+        
+        return CreatedAtAction(
+            nameof(RecuperarFilmePorId),
+            new {id = filme.Id},
+            filme
+            );
     }
 
     [HttpGet]
-    public void ListarFilmesConsole()
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        filmes.ForEach(f => Console.WriteLine($"Filme: {f.Titulo} | Gênero: {f.Genero} | Duração: {f.Duracao}min"));
+        return filmes.Skip(skip).Take(take);
     }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(f => f.Id == id);
+        if(filme is null) return NotFound();
+        return Ok(filme);
+    }
+
 
 }
--- FilmesAPI_Alura/Models/Filme.cs
+++ FilmesAPI_Alura/Models/Filme.cs
@@ -4,13 +4,25 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
-    [Required(ErrorMessage ="O título do filme é obrigatório!")]
-    [MaxLength(100, ErrorMessage ="O tamanho do título não pode exceder 100 caracteres")]
+    public int Id { get; set; }
+
+    [StringLength(50, ErrorMessage = "O tamanho do título diretor não pode exceder 50 caracteres")]
+    [Required(ErrorMessage = "O campo título do filme é obrigatório!")]
     public string Titulo { get; set; }
-    [Required(ErrorMessage = "O gênero do filme é obrigatório!")]
-    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    [Required(ErrorMessage = "O campo gênero do filme é obrigatório!")]
+    [MaxLength(50, ErrorMessage = "O tamanho do campo gênero não pode exceder 50 caracteres")]
     public string Genero { get; set; }
-    [Required(ErrorMessage = "A duração do filme é obrigatória!")]
-    [Range(70,600, ErrorMessage ="A duração do filme deve estar entre 70 e 600 min")]
+    [Required(ErrorMessage = "O campo duração do filme é obrigatório!")]
+    [Range(70,600, ErrorMessage = "O campo duração do filme deve estar entre 70 e 600 min")]
     public int Duracao { get; set; }
+    [Required(ErrorMessage = "O campo diretor do filme é obrigatório!")]
+    [MaxLength(100, ErrorMessage = "O tamanho do campo diretor não pode exceder 100 caracteres")]
+    public string Diretor { get; set; }
+    
+
+    public override string ToString()
+    {
+        return $"{Titulo} | Gênero: {Genero} | Duração: {Duracao}min | Diretor: {Diretor}";
+    }
+
 }
